51df00056a8a77c70514127e27e5f52249ca48df
M. K. V <mvaloy@nmbu.no>
2020-06-04 12:06:14 +0200

    Made a test to check if the herbivore-class raises a KeyError, this is not complete

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -9,3 +9,11 @@ class TestHerbivores:
         """ Test that the class Herbivores creates an instance."""
         h = Herbivores()
         assert isinstance(h, Herbivores)
+
+    def test_if_raises_keyerror(self):
+        """
+        Tests that the Herbivores class raises a KeyError if default parameter names are not used.
+
+        Todo: Denne skal teste at programmet blir avbrutt hvis man gir inn veil key
+        """
+        pass
