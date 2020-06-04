db294fd4627482f53554746ba2fb4889be1a0c69
Christine Brinchmann <christibr@nmbu.no>
2020-06-04 12:41:37 +0200

    Wrote test for fittness-method

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -19,6 +19,7 @@ class TestHerbivores:
         pass
 
     def test_default_value_for_age(self):
+        """Testing default value for age"""
         h = Herbivores()
         assert h.age == 0
         h = Herbivores(age=3)
@@ -26,4 +27,15 @@ class TestHerbivores:
 
     def test_age_setter_cant_be_negative(self):
         pass
+
+    def test_fitness_between_0_and_1(self):
+        """Testing if value of fitness is between 0 and 1"""
+        h = Herbivores()
+        assert 0 <= h.fitness() <= 1
+
+    def test_fitness_0_if_zero_weight(self):
+        """Test if value of fitness is 0 when weight is zero or less"""
+        h = Herbivores()
+        h.weight = 0
+        assert h.fitness() == 0
 
