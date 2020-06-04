7f3522ccadc89b43d7db4d8f4717f93301dad3e6
Christine Brinchmann <christibr@nmbu.no>
2020-06-04 11:08:41 +0200

    Added test file for animals.py

--- biosim/animal.py
+++ biosim/animal.py
@@ -9,7 +9,8 @@ class Herbivores:
                             'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
                             'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
                             'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2, 'omega': 0.4,
-                            'F':10.0, 'DeltaPhiMax': None}
+                            'F': 10.0, 'DeltaPhiMax': None}
+
     def __init__(self, age=0):
         """Create a herbivore with age 0"""
         self.age = age
