f0d7f6b82520045c19351fff14039fd995cd9b06
Christine Brinchmann <christibr@nmbu.no>
2020-06-04 16:33:56 +0200

    Added default parameters for carnivores

--- biosim/animals.py
+++ biosim/animals.py
@@ -8,6 +8,12 @@ default_params_herbi = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
                         'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2, 'omega': 0.4,
                         'F': 10.0, 'DeltaPhiMax': None}
 
+# Default parameters for carnivores:
+default_params_carni = {'w_birth': 6.0, 'sigma_birth': 1.0, 'beta': 0.75,
+                        'eta': 0.125, 'a_half': 40.0, 'phi_age': 0.3,
+                        'w_half': 4.0, 'phi_weight': 0.4, 'mu': 0.4,
+                        'gamma': 0.8, 'zeta': 3.5, 'xi': 1.1, 'omega': 0.8,
+                        'F': 50.0, 'DeltaPhiMax': 10.0}
 
 class Animal:
     """This class will represent an animal."""
