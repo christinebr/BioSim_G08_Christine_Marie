e19cc4ef6125290f03319af38dda5e09fd307deb
M. K. V <mvaloy@nmbu.no>
2020-06-04 11:24:24 +0200

    Made a test to check that Herbivore in animals.py is callable

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from biosim.animals import Herbivores
+
 
 class TestHerbivores:
-    pass
+
+    def test_constructor_default(self):
+        """ Test that the class Herbivores creates an instance."""
+        h = Herbivores()
+        assert isinstance(h, Herbivores)
