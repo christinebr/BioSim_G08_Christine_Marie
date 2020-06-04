baa669e2001095e21093f140f8ef64d92b1dd71e
M. K. V <mvaloy@nmbu.no>
2020-06-04 19:46:33 +0200

    Made the update_weight2 method raise a ValueError if an animal tries to eat and give birth at once, the message is not printed out yet, it does not pass its test, but that might be due to something in the test

--- biosim/animals.py
+++ biosim/animals.py
@@ -89,11 +89,16 @@ class Herbivores:
 
         In the spring animals will eat, then amount of fodder will be 0 or more, in the autumn
         animals will loss weigth, i.e. amount_of_fodder=None
+
+        If an animal gives birth, it will loose weight accordingly.
+        No animal should give birth and eat at the same time.
         """
         params = default_params_herbi
-        if amount_fodder_eaten:
+        if weight_of_newborn is not None and amount_fodder_eaten is not None:
+            raise ValueError('No animal could give birth and eat at the same time')
+        elif amount_fodder_eaten:
             self.weight += params['beta']*amount_fodder_eaten
-        elif weight_of_newborn == None:
+        elif weight_of_newborn is None:
             self.weight -= params['eta']*self.weight
         else:
             self.weight -= params['xi']*weight_of_newborn
@@ -183,3 +188,5 @@ if __name__ == "__main__":
     h4.update_weight2(weight_of_newborn=10)
     print(h4.get_weight())
     print(h4.fitness())
+    h4.update_weight2(weight_of_newborn=20, amount_fodder_eaten=50)
+    print(h4.get_weight())
