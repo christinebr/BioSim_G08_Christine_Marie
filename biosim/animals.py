74470cf33b3e4a7f9a8de5137221e4631ae904ef
M. K. V <mvaloy@nmbu.no>
2020-06-05 12:15:18 +0200

    Relocated the parameters outside __init__

--- biosim/animals.py
+++ biosim/animals.py
@@ -15,22 +15,17 @@ class Animal:
 class Herbivores:
     """This class will represent herbivores."""
 
-    # # Default parameters for herbivores:
-    # _params = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
-    #            'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
-    #            'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
-    #            'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2, 'omega': 0.4,
-    #            'F': 10.0, 'DeltaPhiMax': None}
+    # Default parameters for herbivores:
+    _params = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+               'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
+               'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
+               'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2, 'omega': 0.4,
+               'F': 10.0, 'DeltaPhiMax': None}
 
     def __init__(self, weight, age=0):
         """Create a herbivore with age 0"""
         self.weight = weight
         self.age = age
-        self._params = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
-                        'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
-                        'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
-                        'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2, 'omega': 0.4,
-                        'F': 10.0, 'DeltaPhiMax': None}
 
     def get_weight(self):
         return self.weight
