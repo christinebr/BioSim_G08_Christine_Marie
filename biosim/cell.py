2399e96172102ada4370ed29c98acd320563cd6d
M. K. V <mvaloy@nmbu.no>
2020-06-05 12:13:10 +0200

    Made SingleCell class and subclasses of landscapes.

---
+++ biosim/cell.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+class SingleCell:
+    """
+    Keeps control of amount of animals of both species and fodder, and landscape
+    """
+    fodder = {}
+
+    def __init__(self):
+        self.animals_list = []
+
+    def
+
+class Water(SingleCell):
+    """
+    Represents the water-landscape.
+    """
+    pass
+
+
+class Desert(SingleCell):
+    """
+    Represents the desert-landscape.
+    """
+    pass
+
+
+class Lowland(SingleCell):
+    """
+    Represents the lowland-landscape.
+    """
+    pass
+
+
+class Highland(SingleCell):
+    """
+    Represents the highland_landscape.
+    """
+    pass
