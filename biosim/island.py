cba9c9669ab68f2452364f979e774ed7f77317a4
M. K. V <mvaloy@nmbu.no>
2020-06-06 12:24:06 +0200

    Added method for making sure the island that is provided follows the specifications.

--- biosim/island.py
+++ biosim/island.py
@@ -10,6 +10,20 @@ class TheIsland:
     def __init__(self, landscape_of_cells):
         self.landscapes = landscape_of_cells
 
+    def test_if_island_legal(self):
+        """
+        Test if the island follows the specifications.
+        The string tha specify the island must only contain legal letters, i.e. L, H, W and D
+        All the outermost cells must be of the water type.
+
+        Returns
+        -------
+        Raises ValueError if any of the specifications is violated.
+
+        Todo: This might be better to do in the __init__ section
+        """
+        pass
+
     def annual_cycle(self):
         """
         Makes the year happen.
