8ae400f50a66bef43b25451e9b69294191a30528
Christine Brinchmann <christibr@nmbu.no>
2020-06-07 16:20:09 +0200

    Cleaning up in first_example_sim + adding a second_example_sim.py

--- examples/first_example_sim.py
+++ examples/first_example_sim.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 
 """
 First try on a simulation
+Having the simplest island: W W W
+                            W L W
+                            W W W
+Only having herbivores
+No migration
 """
 
 
@@ -14,81 +19,38 @@ if __name__ == '__main__':
                           ['W', 'L', 'W'],
                           ['W', 'W', 'W']])
 
+    # Initial herbivores on island (only in one cell -> 'L')
     ini_herbs = [{'loc': (1, 1),
                   'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20}
                           for _ in range(10)]
                   }]
 
+    # Initialize the island
     isl = TheIsland(landscape_of_cells=landscape, herbis=ini_herbs)
     print(isl.herbis)
     plt.figure()
     for year in range(20):
-        num_initial = len(isl.herbis[0]['pop'])
-        isl.all_animals_eat(landscape)
-        isl.animals_procreate()
+        num_start_of_year = len(isl.herbis[0]['pop'])
+        isl.all_animals_eat(landscape)   # eating
+        isl.animals_procreate()          # giving birth
         num_after_birth = len(isl.herbis[0]['pop'])
         # migration
-        isl.all_animals_age()
-        isl.all_animals_losses_weight()
-        isl.animals_die()
-        num_after_death = len(isl.herbis[0]['pop'])
-        print(year, num_initial, num_after_birth, num_after_death)
+        isl.all_animals_age()            # aging
+        isl.all_animals_losses_weight()  # weight loss
+        isl.animals_die()                # dying
+        num_end_of_year = len(isl.herbis[0]['pop'])
+        print(year, num_start_of_year, num_after_birth, num_end_of_year)
+
+        # Calculating average weight
         tot_weight = 0
         for herb in isl.herbis[0]['pop']:
             tot_weight += herb['weight']
         avg_weight = tot_weight/len(isl.herbis[0]['pop'])
         print(avg_weight)
-        plt.plot(year, avg_weight, '*')
+
         print(isl.herbis)
+
         # Plotting
-        plt.plot(year, num_after_death, '.')
-    plt.show()
-
-    # A big but small island (only Lowland and Highland)
-    ll = np.array([['W', 'W', 'W', 'W'],
-                   ['W', 'L', 'L', 'W'],
-                   ['W', 'H', 'H', 'W'],
-                   ['W', 'L', 'L', 'W'],
-                   ['W', 'W', 'W', 'W']])
-
-    ini_herbs = [{'loc': (1, 1),
-                  'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20}
-                          for _ in range(10)]
-                  },
-                 {'loc': (2, 1),
-                  'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20}
-                          for _ in range(10)]
-                  }
-                 ]
-
-    isl = TheIsland(landscape_of_cells=landscape, herbis=ini_herbs)
-    print(isl.herbis)
-    plt.figure()
-    cell = 1
-    for year in range(20):
-        num_initial = len(isl.herbis[cell]['pop'])
-        isl.all_animals_eat(landscape)
-        isl.animals_procreate()
-        num_after_birth = len(isl.herbis[cell]['pop'])
-        # migration
-        isl.all_animals_age()
-        isl.all_animals_losses_weight()
-        isl.animals_die()
-        num_after_death = len(isl.herbis[cell]['pop'])
-        print(year, num_initial, num_after_birth, num_after_death)
-        tot_weight = 0
-        for herb in isl.herbis[cell]['pop']:
-            tot_weight += herb['weight']
-        avg_weight = tot_weight/len(isl.herbis[cell]['pop'])
-        print(avg_weight)
+        plt.plot(year, num_end_of_year, '.')
         plt.plot(year, avg_weight, '*')
-        print(isl.herbis)
-        # Plotting
-        plt.plot(year, num_after_death, '.')
     plt.show()
-
-    ini_carns = [{'loc': (1, 1),
-                  'pop': [{'species': 'Carnivore',
-                           'age': 5,
-                           'weight': 20}
-                          for _ in range(40)]}]
