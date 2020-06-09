44c2497f0f01ebaae236f79a49f61fd38b8ab044
M. K. V <mvaloy@nmbu.no>
2020-06-09 14:10:13 +0200

    Made a new example, including both carbis and herbis

--- examples/first_herbi_carni_sim.py
+++ examples/first_herbi_carni_sim.py
@@ -18,15 +18,28 @@ if __name__ == "__main__":
                            'weight': 20}
                           for _ in range(150)]}]
 
+    ini_carns = [{'loc': (10, 10),
+                  'pop': [{'species': 'Carnivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(20)]}]
+
     low = Lowland(ini_herbs[0]['pop'])
+    low.add_new_animals_to_cell(ini_carns[0]['pop'])
     herbi_count = []
-    for year in range(100):
-        herbi_count.append(len(low.herbi_list + low.carni_list))
+    carni_count = []
+    years = list(range(300))
+    for year in years:
+        herbi_count.append(len(low.herbi_list))
+        carni_count.append(len(low.carni_list))
         low.animals_in_cell_eat()
         low.birth()
         low.aging_of_animals()
         low.weight_loss_end_of_year()
         low.death()
     plt.figure()
-    plt.plot([year for year in range(100)], herbi_count)
+
+    plt.plot(years, herbi_count)
+    plt.plot(years, carni_count)
+
     plt.show()
