e103202a8169dc3af07816ac0d55a8e5b4e9cc99
Christine Brinchmann <christibr@nmbu.no>
2020-06-10 13:02:28 +0200

    added method for setting cell and animal parameters in island-class

--- biosim/island.py
+++ biosim/island.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from biosim.cell import SingleCell, Highland, Lowland, Desert  # , Water
+from biosim.cell import SingleCell, Highland, Lowland, Desert, Water
 import textwrap
 
 
@@ -30,6 +30,33 @@ class TheIsland:
             self.animals_on_island = animals_on_island
         else:
             self.animals_on_island = []
+
+    def set_cell_params(self, landscape, new_params=None):
+        """
+        Update the parameters for each cell-type on the island
+        Parameters
+        ----------
+        landscape: [str] landscape type: 'L', 'H', 'D' (or 'W')
+        new_params: [dict] parameters for landscape
+        """
+        if landscape == 'L':
+            Lowland.set_params(new_params)
+        elif landscape == 'H':
+            Highland.set_params(new_params)
+        elif landscape == 'D':
+            Desert.set_params(new_params)
+        elif landscape == 'W':
+            Water.set_params(new_params)
+
+    def set_animal_params(self, speci, new_params=None):
+        """
+        Parameters
+        ----------
+        speci: [str] type of animal: Herbivore or Carnivore
+        new_params: [dict] parameters for animals
+        todo: is this ok? parameters for all animals will change?
+        """
+        Lowland.set_animals_params(speci, new_params)
 
     @staticmethod
     def check_if_island_legal(geogr):
