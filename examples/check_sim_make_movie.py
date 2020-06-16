8294bce6bcc6a49961efe7e17ad676c690fc6206
M. K. V <mvaloy@nmbu.no>
2020-06-16 13:46:00 +0200

    Made the images store every 5th year

--- examples/check_sim_make_movie.py
+++ examples/check_sim_make_movie.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
                                             'DeltaPhiMax': 9.})
     sim.set_landscape_parameters('L', {'f_max': 700})
 
-    sim.simulate(num_years=100, vis_years=1, img_years=10)
+    sim.simulate(num_years=100, vis_years=1, img_years=5)
 
     sim.add_population(population=ini_carns)
-    sim.simulate(num_years=100, vis_years=1, img_years=10)
+    sim.simulate(num_years=100, vis_years=1, img_years=5)
 
     sim.make_movie()
 
