4fedc7949b80420a97db2124928ff80d6eb6b75a
Christine Brinchmann <christibr@nmbu.no>
2020-06-17 14:19:02 +0200

    fixed some more warnings

--- plotting/plot_fitness.py
+++ plotting/plot_fitness.py
@@ -12,22 +12,22 @@ if __name__ == "__main__":
                WWWWW"""
 
     ini_animals = [{'loc': (2, 3),
-                     'pop': [{'species': 'Herbivore',
-                              'age': 5,
-                              'weight': 20} for _ in range(200)]
-                            + [{'species': 'Carnivore',
-                                'age': 5,
-                                'weight': 20} for _ in range(20)]
-                    },
-                   {'loc': (3,3),
                     'pop': [{'species': 'Herbivore',
-                              'age': 5,
-                              'weight': 20} for _ in range(200)]
-                            + [{'species': 'Carnivore',
-                                'age': 5,
-                                'weight': 20} for _ in range(20)]
+                             'age': 5,
+                             'weight': 20} for _ in range(200)]
+                    + [{'species': 'Carnivore',
+                        'age': 5,
+                        'weight': 20} for _ in range(20)]
+                    },
+                   {'loc': (3, 3),
+                    'pop': [{'species': 'Herbivore',
+                             'age': 5,
+                             'weight': 20} for _ in range(200)]
+                    + [{'species': 'Carnivore',
+                        'age': 5,
+                        'weight': 20} for _ in range(20)]
                     }
-                    ]
+                   ]
 
     island = TheIsland(geogr, ini_animals)
 
@@ -35,7 +35,8 @@ if __name__ == "__main__":
     herbi_fitness = [0.9, 0.3, 0.5, 0.4, 0.6, 0.5, 0.7, 0.3, 0.95, 0.2, 0.3, 0.3]
     carni_fitness = [0.6, 0.4, 0.6, 0.5, 0.6, 0.5, 0.5, 0.5]
 
-    plt.hist(herbi_fitness, bins=5, range=(0, 1), histtype='stepfilled', fill=False, edgecolor='red')
+    plt.hist(herbi_fitness, bins=5, range=(0, 1), histtype='stepfilled',
+             fill=False, edgecolor='red')
     plt.hist(carni_fitness, range=(0, 1), histtype='stepfilled', fill=False, edgecolor='blue')
 
     plt.show()
--- plotting/plot_of_sum_animals.py
+++ plotting/plot_of_sum_animals.py
@@ -12,22 +12,22 @@ if __name__ == "__main__":
                WWWWW"""
 
     ini_animals = [{'loc': (2, 3),
-                     'pop': [{'species': 'Herbivore',
-                              'age': 5,
-                              'weight': 20} for _ in range(200)]
-                            + [{'species': 'Carnivore',
-                                'age': 5,
-                                'weight': 20} for _ in range(20)]
-                    },
-                   {'loc': (3,3),
                     'pop': [{'species': 'Herbivore',
-                              'age': 5,
-                              'weight': 20} for _ in range(200)]
-                            + [{'species': 'Carnivore',
-                                'age': 5,
-                                'weight': 20} for _ in range(20)]
+                             'age': 5,
+                             'weight': 20} for _ in range(200)]
+                    + [{'species': 'Carnivore',
+                        'age': 5,
+                        'weight': 20} for _ in range(20)]
+                    },
+                   {'loc': (3, 3),
+                    'pop': [{'species': 'Herbivore',
+                             'age': 5,
+                             'weight': 20} for _ in range(200)]
+                    + [{'species': 'Carnivore',
+                        'age': 5,
+                        'weight': 20} for _ in range(20)]
                     }
-                    ]
+                   ]
 
     island = TheIsland(geogr, ini_animals)
     # Makes the island
