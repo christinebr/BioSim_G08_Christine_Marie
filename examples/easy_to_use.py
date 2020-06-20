902cb3b5fa829d7a5d4ca62742738cb3ad39b7f7
Christine Brinchmann <christibr@nmbu.no>
2020-06-20 11:08:09 +0200

    made some changes to easy_to_use.py

--- examples/easy_to_use.py
+++ examples/easy_to_use.py
@@ -15,18 +15,18 @@ This is an easy form for making the simulation run.
 if __name__ == '__main__':
     plt.ion()
 
-    geogr = """\
+    island = """\
                WWWW
                WLHW
                WLDW
                WWWW"""
-    geogr = textwrap.dedent(geogr)
+    island = textwrap.dedent(island)
 
     ini_herbs = [{'loc': (2, 2),
                   'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20}
                           for _ in range(150)]}]
 
-    sim = BioSim(island_map=geogr, ini_pop=ini_herbs, seed=123456)
+    sim = BioSim(island_map=island, ini_pop=ini_herbs, seed=123456)
 
     # Simulate first 100 years with only herbivores
     sim.simulate(num_years=100, vis_years=1, img_years=1)
